8c8f2533d6dc265dd3bf908dcb7a01ddc5c7faf3
Meredith McIntosh <meredith@mcintosh.io>
2020-09-11 18:56:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,14 +1,14 @@
-# def introduction (name)
-#   puts "Hi, my name is #{name}."
-# end
+def introduction (name)
+  puts "Hi, my name is #{name}."
+end
 
-# introduction ("Kelly")
+introduction ("Kelly")
 
-# def introduction_with_language(name, language)
-#   puts "Hi, my name is #{name} and I am learning to program in #{language}."
-# end
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
-# introduction_with_language("Shannon", "Ruby")
+introduction_with_language("Shannon", "Ruby")
 
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
